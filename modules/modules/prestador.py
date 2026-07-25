3d35dc05e3c14508a73dad97ac130dee315fc1b6
ffaustofortesKK <ffaustofortes@gmail.com>
2026-07-25 14:31:10 +0100

    Create prestador.py

---
+++ modules/modules/prestador.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import qrcode
+from io import BytesIO
+
+def show_prestador_panel(prestador_slug="bfa_w", nome_prestador="bfa w"):
+    # Estilos CSS e toda a estrutura visual dos cartões que criámos acima...
