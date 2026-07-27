8fef41a4d3fd9ab23f69f7be36166577645032a9
ffaustofortesKK <ffaustofortes@gmail.com>
2026-07-27 12:47:29 +0100

    Delete modules/prestador.py

--- modules/prestador.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import qrcode
-from io import BytesIO
-
-def show_prestador_panel(prestador_slug="bfa_w", nome_prestador="bfa w"):
-    # Estilos CSS e toda a estrutura visual dos cartões que criámos acima...
