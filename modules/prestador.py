11f1ad7aafcabd5db8da8eb0396f62a7c1874cf6
ffaustofortesKK <ffaustofortes@gmail.com>
2026-08-10 11:45:28 +0100

    Update prestador.py

--- modules/prestador.py
+++ modules/prestador.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Adiciona a pasta raiz ao caminho do Python para encontrar o config.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import requests
 import time
