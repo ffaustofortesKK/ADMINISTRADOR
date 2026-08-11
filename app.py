b79c3301d4e30aa271504b0a792e15ca8fc7b489
ffaustofortesKK <ffaustofortes@gmail.com>
2026-08-11 21:48:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -970,12 +970,6 @@ def show_client_screen():
 
 
 def show_provider_panel_center(token):
-
-    show_provider_panel_custom(token)
-
-
-
-def show_provider_panel_center(token):
     show_provider_panel_custom(token)
 
 def show_admin_panel():
